bcffb56bf67e51565db1b2f178d1c4109aa90580
Ankit Batra <56373187+ankitbatra22@users.noreply.github.com>
2022-07-26 14:35:29 -0400

    upgrade to discord v13

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ const ytch = require('yt-channel-info')
 const config = require('./config.json')
 const fs = require('fs');
 
-const client = new Discord.Client();
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 console.log('new instance')
 
 client.commands = new Discord.Collection();
@@ -102,6 +104,29 @@ client.on('message', message => {
     else if (command === 'anime') {
       client.commands.get('anime').execute(message, args);
     }
+    else if (command === 'finances') {
+      // send a message to channel that says "Enter the amount you want to split in $" and then store their response in a variable called money
+      message.channel.send("Enter the amount you want to split in $")
+      const filter = m => m.author.id === message.author.id;
+      message.channel.awaitMessages(filter, { max: 1, time: 60000, errors: ['time'] })
+      .then(collected => {
+        let money = collected.first().content;
+        // The money needs to be split into 80/10/5/5
+        // savings, investements, chequings, goals
+        // Send an embeded message that says "Your savings, investements, chequings, goals are: "
+        const embed = new Discord.MessageEmbed();
+        embed.setColor('#0099ff');
+        embed.setTitle('Your Finances');
+        embed.setDescription(`Your savings, investements, chequings, goals are:`);
+        embed.addField('Savings', `${money*0.8}`);
+        embed.addField('Investements', `${money*0.1}`);
+        embed.addField('Chequings', `${money*0.05}`);
+        embed.addField('Goals', `${money*0.05}`);
+        message.channel.send(embed);
+        
+    }
+    )
+  }
 
 
 });
